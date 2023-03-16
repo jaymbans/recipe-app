d50367efd18098444671bc8ad1ed4f98a7da9558
jay mbans <jordan@mbanefo.com>
2023-03-15 23:09:48 -0400

    nav link highlight

--- ingreedients/src/components/NavigationBar.js
+++ ingreedients/src/components/NavigationBar.js
@@ -5,16 +5,36 @@ import {
   NavLinkContainer
 }
   from '../StyledComponents';
-
+import { useState, useEffect } from 'react'
 
 
 function NavigationBar() {
 
-  const currentPageChange = (e) => {
+  useEffect(() => {
+
+    const currentPage = document.location.hash.split('/')[document.location.hash.split.length - 1];
+
     const navLinks = Array.from(document.querySelectorAll('.nav-link'));
     navLinks.forEach(link => link.classList.remove('active'));
-    e.target.classList.add('active')
-  }
+
+    switch (currentPage) {
+      case '':
+        document.getElementById('home-link').classList.add('active');
+        break;
+      case 'recipes':
+        document.getElementById('recipes-link').classList.add('active');
+        break;
+      case 'add-recipes':
+        document.getElementById('add-recipes-link').classList.add('active');
+        break;
+      case 'profile':
+        document.getElementById('profile-link').classList.add('active');
+        break;
+      default:
+        return;
+    }
+
+  }, [])
 
   return (
     <NavigationContainer>
@@ -23,10 +43,10 @@ function NavigationBar() {
         InGredients
       </LogoContainer>
       <NavLinkContainer>
-        <NavLink to='/' onClick={currentPageChange} className='nav-link'>Home</NavLink>
-        <NavLink to='/recipes' onClick={currentPageChange} className='nav-link'>My Recipes</NavLink>
-        <NavLink onClick={currentPageChange} className='nav-link'>Add Recipes</NavLink>
-        <NavLink onClick={currentPageChange} className='nav-link'>Profile</NavLink>
+        <NavLink id='home-link' to='/' className='nav-link'>Home</NavLink>
+        <NavLink id='recipes-link' to='/recipes' className='nav-link'>My Recipes</NavLink>
+        <NavLink id='add-recipes-link' className='nav-link'>Add Recipes</NavLink>
+        <NavLink id='profile-link' className='nav-link'>Profile</NavLink>
       </NavLinkContainer>
     </NavigationContainer>
   )
